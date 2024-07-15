0f3c06dda55fbe0760f935359592ecf24dead229
Vincent Limo <limovincenti@gmail.com>
2024-07-15 09:36:15 +0300

    improve the looks of the cards, add atuoloading and remove unncesary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import {useState} from 'react';
-import words from "./../data/traslations.json"
+import {useState, useEffect, useCallback} from 'react';
+import words from "./data/translationsV1.json";
 
 function App() {
-    const [data, setData] = useState(words);
+    const [data, _] = useState(words);
     const [searchTerm, setSearchTerm] = useState('');
     const [filter, setFilter] = useState('');
-    const [visibleCount, setVisibleCount] = useState(20);
-
-    const loadMoreButtonRef = useRef(null);
-    const loadMoreTimeoutRef = useRef(null);
+    const [visibleCount, setVisibleCount] = useState(21);
+    const [loading, setLoading] = useState(false);
 
     const filteredData = data
         .filter(item => item.word.toLowerCase().includes(searchTerm.toLowerCase()))
@@ -16,42 +14,35 @@ function App() {
 
     const partsOfSpeech = [...new Set(words.map(item => item.partOfSpeech))].sort((a, b) => a.localeCompare(b));
 
-    const loadMore = () => {
-        setVisibleCount(prevCount => prevCount + 20);
-    };
+    const loadMore = useCallback(() => {
+        if (!loading) {
+            setLoading(true);
+            setTimeout(() => {
+                setVisibleCount(prevCount => prevCount + 21);
+                setLoading(false);
+            }, Math.random() * 1000 + 500);
+        }
+    }, [loading]);
 
-    const handleUserInteraction = () => {
-        if (loadMoreTimeoutRef.current) {
-            clearTimeout(loadMoreTimeoutRef.current);
+    const handleScroll = useCallback(() => {
+        if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || loading) {
+            return;
         }
         loadMore();
-    };
+    }, [loadMore, loading]);
 
     useEffect(() => {
-        const observer = new IntersectionObserver((entries) => {
-            if (entries[0].isIntersecting) {
-                loadMoreTimeoutRef.current = setTimeout(() => {
-                    loadMore();
-                }, 3000); // Adjust the delay as needed (e.g., 3000ms = 3 seconds)
-            }
-        });
-
-        if (loadMoreButtonRef.current) {
-            observer.observe(loadMoreButtonRef.current);
-        }
-
+        window.addEventListener('scroll', handleScroll);
         return () => {
-            if (loadMoreButtonRef.current) {
-                observer.unobserve(loadMoreButtonRef.current);
-            }
-            if (loadMoreTimeoutRef.current) {
-                clearTimeout(loadMoreTimeoutRef.current);
-            }
+            window.removeEventListener('scroll', handleScroll);
         };
-    }, [loadMoreButtonRef.current]);
+    }, [handleScroll]);
 
     return (
-        <div className="min-h-screen bg-gray-100 p-8">
+        <div className="min-h-screen bg-gray-100 p-4 sm:p-6">
+            <h1 className="text-3xl font-bold text-center p-3 mb-3">
+                {data.length.toLocaleString()} Commonly used Finnish Words
+            </h1>
             <div className="max-w-xl mx-auto mb-6">
                 <input
                     type="text"
@@ -61,53 +52,67 @@ function App() {
                     onChange={(e) => setSearchTerm(e.target.value)}
                 />
             </div>
-            <div className="mx-auto w-auto mb-6">
+            <div className="mx-auto w-full mb-6">
                 <div className="flex flex-wrap justify-center">
                     <button
-                        className={`flex-shrink-0 w-full sm:w-auto px-4 py-2 rounded ${filter === '' ? 'bg-blue-500 text-white' : 'bg-white text-blue-500 border border-blue-500'}`}
+                        className={`flex w-full m-0.5 sm:w-auto p-1 text-xs rounded ${filter === '' ? 'bg-blue-500 text-white' : 'bg-white text-blue-500 border border-blue-500'}`}
                         onClick={() => setFilter('')}
                     >
-                        All ({words.length})
+                        All ({words.length.toLocaleString()})
                     </button>
                     {partsOfSpeech.map((part, index) => (
                         <button
                             key={index}
-                            className={`flex-shrink-0 w-full sm:w-auto p-1 text-xs rounded ${filter === part ? 'bg-blue-500 text-white' : 'bg-white text-blue-500 border border-blue-500'}`}
+                            className={`flex w-full m-0.5 sm:w-auto p-1 text-xs rounded ${filter === part ? 'bg-blue-500 text-white' : 'bg-white text-blue-500 border border-blue-500'}`}
                             onClick={() => setFilter(part)}
                         >
-                            {part.charAt(0).toUpperCase() + part.slice(1)} ({words.filter(word => word.partOfSpeech === part).length})
+                            {part.charAt(0).toUpperCase() + part.slice(1)} ({words.filter(word => word.partOfSpeech === part).length.toLocaleString()})
                         </button>
                     ))}
                 </div>
             </div>
-            <div className="flex flex-wrap justify-center gap-6">
+            <div className="flex flex-wrap justify-center gap-4 md:gap-6">
                 {filteredData.slice(0, visibleCount).map((item, index) => (
-                    <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 rounded overflow-hidden shadow-lg bg-white transform transition-transform hover:scale-105">
-                        <div className="px-6 py-4">
-                            <div className="flex items-center justify-between mb-2">
-                                <span className="text-2xl text-blue-700 mr-2">{item.word}</span>
-                                <span className="text-gray-500 text-sm">{item.partOfSpeech.charAt(0).toUpperCase() + item.partOfSpeech.slice(1)}</span>
+                    <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/4 mb-4">
+                        <div
+                            className="h-full rounded overflow-hidden shadow-lg bg-white transform transition-transform hover:scale-105 flex flex-col">
+                            <div className="px-4 py-3 flex-grow">
+                                <div className="flex items-center justify-between mb-2">
+                                    <span className="text-lg text-blue-700 font-semibold">{item.word}</span>
+                                    <span
+                                        className="text-gray-500 text-sm">{item.partOfSpeech.charAt(0).toUpperCase() + item.partOfSpeech.slice(1)}</span>
+                                </div>
+                                <p className="text-gray-700 text-base mb-2">{item.definition}</p>
+                                <p className="text-gray-700 italic text-base">
+                                    <svg className="inline-block w-4 h-4 mr-2 fill-current text-gray-500"
+                                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path
+                                            d="M10 0C4.486 0 0 4.486 0 10s4.486 10 10 10 10-4.486 10-10S15.514 0 10 0zm1 14H9v-2h2v2zm0-4H9V5h2v5z"/>
+                                    </svg>
+
+                                    <span className="text-blue-600 text-base">
+                                         {item.example.slice(0, item.example.indexOf("("))}
+                                    </span>
+                                    <span className="text-gray-700 italic text-base">
+                                    - {item.example.slice(item.example.indexOf("(") + 1, item.example.indexOf(")"))}
+                                    </span>
+                                </p>
                             </div>
-                            <p className="text-gray-700 text-base mb-3">{item.definition}</p>
-                            <p className="text-gray-700 text-base">
-                                <svg className="inline-block w-4 h-4 mr-2 fill-current text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path d="M10 0C4.486 0 0 4.486 0 10s4.486 10 10 10 10-4.486 10-10S15.514 0 10 0zm1 14H9v-2h2v2zm0-4H9V5h2v5z"/>
-                                </svg>
-                                {item.example}
-                            </p>
                         </div>
                     </div>
                 ))}
             </div>
-            {visibleCount < filteredData.length && (
+
+
+            {loading && (
                 <div className="flex justify-center mt-6">
-                    <button
-                        ref={loadMoreButtonRef}
-                        onClick={handleUserInteraction}
-                        className="px-6 py-2 bg-blue-500 text-white rounded shadow-md hover:bg-blue-600 transition"
-                    >
-                        Load More
-                    </button>
+                    <svg className="animate-spin h-5 w-5 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none"
+                         viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                strokeWidth="4"></circle>
+                        <path className="opacity-75" fill="currentColor"
+                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.964 7.964 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
                 </div>
             )}
         </div>
